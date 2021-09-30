e51bc72e2b3bb3eeb94805afbb8e7e011b4f3aac
R0DlON <rodionprokopchenkoo@gmail.com>
2021-09-30 19:32:04 +0300

    replace

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './sass/main.scss';
+
+import menu from './templates/gallery-items.hbs';
+
+import menuItems from './menu.json';
+
+const placeForMenuItems = document.querySelector("ul[class='menu js-menu']");
+placeForMenuItems.insertAdjacentHTML('beforeend', createMenuItems(menuItems));
+function createMenuItems(menuItems) {
+  return menu(menuItems);
+}
+
+const buttonOfTheme = document.querySelector('#theme-switch-toggle');
+buttonOfTheme.addEventListener('change', switchTheme);
+// const saveThemeAfterReload = {};
+function switchTheme(e) {
+  const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+  if (document.body.classList.contains(Theme.LIGHT)) {
+    document.body.classList.remove(Theme.LIGHT);
+    document.body.classList.add(Theme.DARK);
+
+    let currentTheme = document.body.className;
+    localStorage.setItem('theme', currentTheme);
+    return;
+  }
+
+  document.body.classList.remove(Theme.DARK);
+  document.body.classList.add(Theme.LIGHT);
+  let currentTheme = document.body.className;
+  localStorage.setItem('theme', currentTheme);
+}
+console.log(buttonOfTheme);
+document.body.classList.add(localStorage.getItem('theme'));
